9f52b5818eb2425a5fc17902c4acdc7a9fe628df
xdatk <fq19930317@gmail.com>
2015-03-21 20:59:13 +0800

    添加文件上传翻译

---
+++ resources/lang/en/file.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XdaTk
+ * Date: 2015/3/21
+ * Time: 19:12
+ */
+return [
+    'fileNotFound' => 'Upload File Not Found',
+    'fileUploadError' => 'File Upload Error',
+    'fileSuccess' => 'File Upload Success',
+];
---
+++ resources/lang/zh_CN/file.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XdaTk
+ * Date: 2015/3/21
+ * Time: 19:12
+ */
+return [
+    'fileNotFound' => '没有找到要上传的文件',
+    'fileUploadError' => '上传文件错误',
+    'fileSuccess' => '文件上传成功',
+    'fileSaveError' => '文件保存失败',
+    'fileTypeError' => '文件类型错误',
+];
