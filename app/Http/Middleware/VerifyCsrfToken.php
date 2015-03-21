5033d66cd0a9f0e86000fbe559c79e9403e9460c
xdatk <fq19930317@gmail.com>
2015-03-21 20:55:16 +0800

    调整CSRF验证，避免出现文件上传之类的BUG

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -5,6 +5,28 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
+    /**
+     * Routes we want to exclude.
+     *
+     * @var array
+     */
+    protected $routes = [
+        'edit/editorFileUpload',
+    ];
+
+    /**
+     * This will return a bool value based on route checking.
+     * @param  Request $request
+     * @return boolean
+     */
+    protected function excludedRoutes($request)
+    {
+        foreach ($this->routes as $route)
+            if ($request->is($route))
+                return true;
+
+        return false;
+    }
 
     /**
      * Handle an incoming request.
@@ -13,8 +35,14 @@ class VerifyCsrfToken extends BaseVerifier
      * @param  \Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, \Closure $next)
     {
+        if ($this->isReading($request)
+            || $this->excludedRoutes($request)
+            || $this->tokensMatch($request)
+        ) {
+            return $this->addCookieToResponse($request, $next($request));
+        }
         return parent::handle($request, $next);
     }
 
