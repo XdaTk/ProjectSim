05bfd6ac9e5da80e6140c304c683199825aed529
xdatk <fq19930317@gmail.com>
2015-04-05 20:05:05 +0800

    优化代码

--- app/Http/Controllers/Util/EditorFileUploadController.php
+++ app/Http/Controllers/Util/EditorFileUploadController.php
@@ -11,6 +11,23 @@ class EditorFileUploadController extends Controller
     protected $savePath = '/upload';
 
     /**
+     * Editor File Upload
+     * @return string
+     */
+    public function image()
+    {
+        $message = new FileMessage();
+        $file = $this->hasFile();
+        if ($file != null && $this->fileCheck($file) == null) {
+            $message = $this->fileSave($file);
+        } else {
+            $message->success = FileMessage::$UPFAIL;
+            $message->message = $this->fileCheck($file);
+        }
+        return json_encode($message);
+    }
+
+    /**
      * Check File Has
      */
     protected function hasFile()
@@ -28,8 +45,7 @@ class EditorFileUploadController extends Controller
      * @param $file
      * @return string
      */
-    protected
-    function fileCheck($file)
+    protected function fileCheck($file)
     {
         if ($file->isValid()) {
             $v = Validator::make(['file' => $file], array(
@@ -49,8 +65,7 @@ class EditorFileUploadController extends Controller
      * @param $file
      * @return FileMessage
      */
-    protected
-    function fileSave($file)
+    protected function fileSave($file)
     {
         $message = new FileMessage();
         try {
@@ -68,23 +83,5 @@ class EditorFileUploadController extends Controller
             $message->message = \Lang::get('file.fileSaveError');
             return $message;
         }
-    }
-
-    /**
-     * Editor File Upload
-     * @return string
-     */
-    public
-    function image()
-    {
-        $message = new FileMessage();
-        $file = $this->hasFile();
-        if ($file != null && $this->fileCheck($file) == null) {
-            $message = $this->fileSave($file);
-        } else {
-            $message->success = FileMessage::$UPFAIL;
-            $message->message = $this->fileCheck($file);
-        }
-        return json_encode($message);
     }
 }
